1fe74a3fd060c107d35ad0ff3d8e35643f4a4676
Sagar Gupta <guptasagar012@gmail.com>
2026-07-12 18:53:17 -0400

    Components Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Contact } from './contact/contact';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'contact',
+        component: Contact
+    },
+    {
+        path: 'about',
+        component: About
+    }
+];
